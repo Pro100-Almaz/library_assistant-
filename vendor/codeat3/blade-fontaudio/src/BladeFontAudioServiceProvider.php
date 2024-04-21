d25b2c8c8f04b7b3fd3294865d9e060d964505f8
almaz amanzholuly <almaz.amanzholuly@nu.edu.kz>
2024-04-21 16:09:41 +0500

    added: text-to-audio

---
+++ vendor/codeat3/blade-fontaudio/src/BladeFontAudioServiceProvider.php
@@ -0,0 +1,41 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Codeat3\BladeFontAudio;
+
+use BladeUI\Icons\Factory;
+use Illuminate\Contracts\Container\Container;
+use Illuminate\Support\ServiceProvider;
+
+final class BladeFontAudioServiceProvider extends ServiceProvider
+{
+    public function register(): void
+    {
+        $this->registerConfig();
+
+        $this->callAfterResolving(Factory::class, function (Factory $factory, Container $container) {
+            $config = $container->make('config')->get('blade-fontaudio', []);
+
+            $factory->add('font-audio', array_merge(['path' => __DIR__ . '/../resources/svg'], $config));
+        });
+    }
+
+    private function registerConfig(): void
+    {
+        $this->mergeConfigFrom(__DIR__ . '/../config/blade-fontaudio.php', 'blade-fontaudio');
+    }
+
+    public function boot(): void
+    {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__ . '/../resources/svg' => public_path('vendor/blade-fontaudio'),
+            ], 'blade-fontaudio');
+
+            $this->publishes([
+                __DIR__ . '/../config/blade-fontaudio.php' => $this->app->configPath('blade-fontaudio.php'),
+            ], 'blade-fontaudio-config');
+        }
+    }
+}
